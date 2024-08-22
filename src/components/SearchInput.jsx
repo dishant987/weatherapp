9ebbfc85bda779197347735745ce205773844272
dishant987 <tankdishant2003@gmail.com>
2024-08-22 15:30:14 +0530

    change

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -14,7 +14,7 @@ const SearchInput = ({ onSelectCity }) => {
             const fetchSuggestions = async () => {
                 try {
                     const response = await axios.get(
-                        `http://api.openweathermap.org/geo/1.0/direct`,
+                        `https://api.openweathermap.org/geo/1.0/direct`,
                         {
                             params: {
                                 q: input,
